2556f3f6089c408e74e5a3f29b9a696a070dfcdb
Martin <martin.lundquist@hotmail.com>
2018-06-05 12:08:55 +0200

    Update js.js

--- js.js
+++ js.js
@@ -22,11 +22,18 @@ $(document).ready(function () {
             width: "50px"
         }, 100);
     });
-    $(document).mousemove(function(e){
-        var $width = ($(document).width())/255;
-        var $height = ($(document).height())/255;
-        var $pageX = parseInt(e.pageX / $width,10);
-        var $pageY = parseInt(e.pageY / $height,10);
-        $("#omMig").css("color", "rgb("+50+$pageX+","+$pageX+","+$pageY+")");
-    });
 });
+
+/***
+ * Färgfunktionen
+ ***/
+$(document).mousemove(function (e) {
+    var $width = ($(document).width()) / 255;
+    var $height = ($(document).height()) / 255;
+    var $pageX = parseInt(e.pageX / $width, 10);
+    var $pageY = parseInt(e.pageY / $height, 10);
+    $("#omMig").css("color", "rgb(" + 30 + $pageX + "," + $pageX + "," + $pageY + ")");
+    $("#omMig").css("background-color", "rgba(" + $pageY + "," + (255 - $pageX) + "," + (255 - $pageY) + "," + 0.6 + ")");
+
+});
+
